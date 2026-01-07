80addb6711d325d4f880064caa63edcb17ff781b
isixe <isixe@outlook.com>
2026-01-07 22:08:57 +0800

    🔧 chore(public/data): Add new border and shape JSON style data

--- src/components/widget/style-shape.tsx
+++ src/components/widget/style-shape.tsx
@@ -49,7 +49,7 @@ export function StyleShape({ index, style, tab }: StyleBoxProps) {
 
 	return (
 		<div
-			className={`${uniqueClassName} group aspect-square rounded-lg bg-white flex items-center justify-center text-gray-600 font-medium transition-transform duration-200 hover:scale-105 relative cursor-pointer`}
+			className={`${uniqueClassName} group aspect-square rounded-lg flex items-center justify-center text-gray-600 font-medium transition-transform duration-200 hover:scale-105 relative cursor-pointer`}
 			onClick={onStyleCopy}>
 			<span className="z-10 text-[16px] font-normal text-gray-400">#{index}</span>
 
